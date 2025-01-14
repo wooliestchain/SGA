31391671525e08299414e036fe5227f850b04cd4
wooliestchain <levyren38@gmail.com>
2025-01-14 22:25:19 +0100

    1

--- src/api/projet.rs
+++ src/api/projet.rs
@@ -22,7 +22,7 @@ pub async fn ajouter_projet (form: web::Json<Projet>) -> impl Responder{
 
         let query = r#"
         INSERT INTO Projet(nom, description, ministere_responsable, referant_projet, referant_presidence, annee_debut, annee_fin, type_projet, source_financement,statut, priorite, objectifs, impact_attendu)
-        VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+        VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
         RETURNING id;
         "#;
 
