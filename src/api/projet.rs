726806b0fa80117b8bf7b22e834c9e451610c339
wooliestchain <levyren38@gmail.com>
2025-01-15 19:59:11 +0100

    1

--- src/api/projet.rs
+++ src/api/projet.rs
@@ -20,6 +20,7 @@ pub async fn ajouter_projet (form: web::Json<Projet>) -> impl Responder{
             "Erreur de connexion à la base de données"
         })?;
 
+        //Modifier Projet pour y ajouter code, établir des inter médiations pour d'autres colonnes
         let query = r#"
         INSERT INTO Projet(nom, description, ministere_responsable, referant_projet, referant_presidence, annee_debut, annee_fin, type_projet, source_financement,statut, priorite, objectifs, impact_attendu)
         VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
