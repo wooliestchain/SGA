d83c0b90ddc06279c09150eaa9d41e00376ef38f
wooliestchain <levyren38@gmail.com>
2025-01-24 19:21:50 +0100

    1

--- src/api/intermediary.rs
+++ src/api/intermediary.rs
@@ -20,7 +20,7 @@ pub async fn categorie_add(form: web::Json<projet::Categorie>) -> impl Responder
         })?;
 
         let query = r#"
-        INSERT INTO Categorie(nom) VALUES ($1) RETURNING id;
+        INSERT INTO categorie(nom) VALUES ($1) RETURNING id;
         "#;
 
         match client.query_one(query, &[&categorie.nom]){
@@ -42,4 +42,36 @@ pub async fn categorie_add(form: web::Json<projet::Categorie>) -> impl Responder
         Err(_) => HttpResponse::InternalServerError().body("Erreur interne du serveur")
     }
 
+}
+
+pub async fn ville_add (form : web::Json<projet::Ville>) -> impl Responder{
+    let ville = form.into_inner();
+    let result = tokio::task::spawn_blocking(move ||{
+
+        let mut client  = database_connexion().map_err(|err|{
+            eprintln!("Erreur de connexion à la base de données {:?}", err);
+            "Erreur de connexion à la base de données"
+        })?;
+
+        let query = r#"
+        INSERT INTO ville(nom) VALUES ($1) RETURNING id;
+        "#;
+        match client.query_one(query, &[&ville.nom]){
+            Ok(row) => {
+                let id: i32 = row.get("id");
+                Ok(format!("Ville ajoutée avec l'ID: {}", id))
+            }
+            Err(err) => {
+                eprintln!("Erreur lors de l'ajout de la ville {:?}", err);
+                Err("Erreur lors de l'ajout du projet")
+            }
+        }
+    })
+    .await;
+
+    match result{
+        Ok(Ok(response)) => HttpResponse::Ok().json(response),
+        Ok(Err(err)) => HttpResponse::InternalServerError().body(err),
+        Err(_) => HttpResponse::InternalServerError().body("Erreur interne du serveur")
+    }
 }
